25fc9941773f1c123a2fd0a98bb650b9f9f2eda7
ViribusCaptiosus <48342884+ViribusCaptiosus@users.noreply.github.com>
2021-03-15 06:59:01 -0500

    cleaned up code and added working disengagement system

--- main.cpp
+++ main.cpp
@@ -3,25 +3,30 @@
 #include <iostream>
 #include <math.h>
 
-//#include <chrono>
 
 int color_sum(HDC device_handle, int x, int y) {
+    // get COLORREF data from windows api
     COLORREF rgb = GetPixel(device_handle, x, y);
+    // return sum of r, g, and b channel values
     return GetRValue(rgb) + GetGValue(rgb) + GetBValue(rgb);
 }
 
+
 void click() {
     INPUT Input = { 0 };
+
     // mouse down
     Input.type = INPUT_MOUSE;
     Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
     SendInput(1, &Input, sizeof(INPUT));
+
     // mouse up
     ZeroMemory(&Input, sizeof(INPUT));
     Input.type = INPUT_MOUSE;
     Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
     SendInput(1, &Input, sizeof(INPUT));
 }
+
 
 int main() {
 
@@ -39,13 +44,17 @@ int main() {
     int check_x = WIDTH / 2 + 2;
     int check_y = HEIGHT / 2 + 2;
 
-    // int key = 0x46; // f key
-    int key = 0x43;
-    int start_color_sum, last_color_sum;
-    int threshold = 20;
-    //int key = 0x43; // c key
-    //std::cout << GetAsyncKeyState(key) << std::endl;
+    // macro settings
+    int key = 0x54; // 't' key virtual key code
+    int threshold = 30;
 
+    // initialize disengagement boolean
+    bool disengaged = 0;
+
+    // define sum variables
+    int start_color_sum, last_color_sum;
+
+    //macro loop
     while (1) {
         // wait for key to be pressed and released
         while (!GetAsyncKeyState(key)) {}
@@ -56,27 +65,24 @@ int main() {
 
         // wait for either the key to be pressed again or the color sum of the sample pixel to change passed the threshold
         last_color_sum = color_sum(device_handle, check_x, check_y);
-        while (!GetAsyncKeyState(key) && std::abs(last_color_sum - start_color_sum) < threshold) {
+        disengaged = 0;
+        while (std::abs(last_color_sum - start_color_sum) < threshold) {
             last_color_sum = color_sum(device_handle, check_x, check_y);
+            if (GetAsyncKeyState(key)) {
+                disengaged = 1;
+                break;
+            }
         }
 
-        // click
-        click();
+        if (disengaged) {
+            // wait for the key to be released before continuing macro loop
+            while (GetAsyncKeyState(key)) {}
+        } else {
+            // click
+            click();
+        }
 
     }
-
-    //using std::chrono::high_resolution_clock;
-    //using std::chrono::duration_cast;
-    //using std::chrono::duration;
-    //using std::chrono::milliseconds;
-    //auto t1 = high_resolution_clock::now();
-    //color_sum(device_handle, check_x, check_y);
-    //click();
-    //auto t2 = high_resolution_clock::now();
-    //auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    //duration<double, std::milli> ms_double = t2 - t1;
-    //std::cout << ms_int.count() << "ms\n";
-    //std::cout << ms_double.count() << "ms\n";
 
     return 0;
 }
