f35150dcf2b35b37a97073ee146ca469e6c4c795
ViribusCaptiosus <48342884+ViribusCaptiosus@users.noreply.github.com>
2021-04-25 22:01:07 -0500

    revised color_selector.cpp

--- color_selector.cpp
+++ color_selector.cpp
@@ -1,14 +1,16 @@
 #include <windows.h>
 #include <wtypes.h>
 #include <fstream>
+#include <string>
 #include <stack>
+#include <iostream>
 
 
 // number of possible values per channel
-const int BRANCH_FACTOR = 85;
+const int BRANCH_FACTOR = 256;
 
 
-void get_pixel_values(HDC device_handle, int x, int y, int *r, int *g, int *b) {
+void get_pixel_values(HDC device_handle, int x, int y, int* r, int* g, int* b) {
     COLORREF rgb = GetPixel(device_handle, x, y);
     *r = GetRValue(rgb);
     *g = GetGValue(rgb);
@@ -17,22 +19,22 @@ void get_pixel_values(HDC device_handle, int x, int y, int *r, int *g, int *b) {
 
 
 struct Trie {
-   struct Trie *child[BRANCH_FACTOR];
-   int r = 0;
-   int g = 0;
-   int b = 0;
+    struct Trie* child[BRANCH_FACTOR];
+    int r = 0;
+    int g = 0;
+    int b = 0;
 };
 
 
-struct Trie *create_node() {
-   struct Trie *node = new Trie;
-   for (int i = 0; i < BRANCH_FACTOR; i++)
-      node->child[i] = NULL;
-   return node;
+struct Trie* create_node() {
+    struct Trie* node = new Trie;
+    for (int i = 0; i < BRANCH_FACTOR; i++)
+        node->child[i] = NULL;
+    return node;
 }
 
 
-void insert(struct Trie *root, int r, int g, int b) {
+void insert(struct Trie* root, int r, int g, int b) {
     if (!root->child[r]) {
         root->child[r] = create_node();
     }
@@ -48,7 +50,7 @@ void insert(struct Trie *root, int r, int g, int b) {
 }
 
 
-bool search(struct Trie *root, int r, int g, int b) {
+bool search(struct Trie* root, int r, int g, int b) {
     if (!root->child[r]) {
         return false;
     }
@@ -62,40 +64,47 @@ bool search(struct Trie *root, int r, int g, int b) {
 }
 
 
-struct Trie *load_trie(int *n, string filename) {
-    int n, r, g, b;
-    std::ifstream in_file (filename);
-    in_file >> n;
-    struct Trie *root = create_node();
-    for (int i = 0; i < n; i++) {
+struct Trie* load_trie(int* n, std::string filename) {
+    std::cout << "Loading trie data from " << filename << "..." << std::endl;
+    int r, g, b;
+    std::ifstream in_file(filename);
+    in_file >> *n;
+    std::cout << "Loading " << *n << " color value(s) into trie datastructure..." << std::endl;
+    struct Trie* root = create_node();
+    for (int i = 0; i < *n; i++) {
         in_file >> r >> g >> b;
+        std::cout << "\t" << r << " " << g << " " << b << std::endl;
         insert(root, r, g, b);
     }
-    in_file.close()
+    in_file.close();
     return root;
 }
 
 
-void save_trie(int n, struct Trie *root) {
+void save_trie(int n, struct Trie* root, std::string filename) {
+    std::cout << "Saving " << n << " color values to " << filename << "..." << std::endl;
     std::ofstream out_file(filename);
-    out_file << n << " ";
-    std::stack<struct Trie *> trie_stack;
+    out_file << n;
+    std::stack<struct Trie*> trie_stack;
     trie_stack.push(root);
-    struct Trie *node;
+    struct Trie* node;
+    bool children = 0;
     while (!trie_stack.empty()) {
         node = trie_stack.top();
         trie_stack.pop();
-        if (node->r) {
-            out_file << " " << node->r << " " << node->g << " " << node->b;
-        } else {
-            for (int i = 0; i < BRANCH_FACTOR; i++) {
-                if (node->child[i]) {
-                    trie_stack.push(node->child[i]);
-                }
+        children = 0;
+        for (int i = 0; i < BRANCH_FACTOR; i++) {
+            if (node->child[i] != NULL) {
+                trie_stack.push(node->child[i]);
+                children = 1;
             }
         }
+        if (!children) {
+            std::cout << "\t" << node->r << " " << node->g << " " << node->b << std::endl;
+            out_file << " " << node->r << " " << node->g << " " << node->b;
+        }
     }
-    out_file.close()
+    out_file.close();
 }
 
 
@@ -103,7 +112,7 @@ int main() {
 
     // load trie data
     int n;
-    struct Trie *values_tree = load_trie(&n, "data.txt");
+    struct Trie* values_tree = load_trie(&n, "data.txt");
 
     // get window and device handles
     HWND desktop_window = GetDesktopWindow();
@@ -133,17 +142,18 @@ int main() {
             // get pixel values and insert them into values tree
             get_pixel_values(device_handle, check_x, check_y, &r, &g, &b);
             if (!search(values_tree, r, g, b)) {
+                std::cout << "Found new color! Inserting " << r << " " << g << " " << b << " into trie datastructure..." << std::endl;
                 insert(values_tree, r, g, b);
-                n ++;
+                n++;
             }
         }
 
         // check for user save input
         if (GetAsyncKeyState(save_key)) {
             // save trie data
-            save_trie(n, values_tree);
+            save_trie(n, values_tree, "data.txt");
             // wait for key to be released before continuing
-            while (!GetAsyncKeyState(save_key)) {}
+            while (GetAsyncKeyState(save_key)) {}
         }
 
     }
